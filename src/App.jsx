fad1c141062aec5d66188d6060b8dbef200cf280
Viper <lmi25@txstate.edu>
2026-03-30 17:43:18 -0500

    Added tailwindcss utility classes to various elements and applied a flexbox to the main container.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 export default function Newsletter() {
     return (
-        <fragment className="flex flex-col justify-center items-center min-h-screen p-5">
+        <fragment className="flex flex-col justify-center items-center min-h-screen p-5 gap-5">
             <header className="w-full">
                 <div className="img-container">
                     <img src="assets/images/illustration-sign-up-mobile.svg" alt="" aria-hidden="true"/>
@@ -8,35 +8,36 @@ export default function Newsletter() {
                 <h1 className="text-left">Stay updated!</h1>
             </header>
 
-            <main>
-            <p>Join 60,000+ product managers receiving monthly updates on:</p>
+            <main className="flex flex-col gap-5">
+                <p>Join 60,000+ product managers receiving monthly updates on:</p>
 
-            <ul>
-                <li>Product discovery and building what matters</li>
-                <li>Measuring to ensure updates are a success</li>
-                <li>And much more!</li>
-            </ul>
+                <ul>
+                    <li>Product discovery and building what matters</li>
+                    <li>Measuring to ensure updates are a success</li>
+                    <li>And much more!</li>
+                </ul>
 
-            <label htmlFor="email">Email address</label>
-            <br />
-            <input id="email" type="text" placeholder="email@company.com" />
-            <br />
+                <div>
+                    <label htmlFor="email">Email address</label>
+                    <input id="email" className="w-full border-1 rounded-md pl-5 py-4" type="text" placeholder="email@company.com" />
+                    <br />
+                </div>
 
-            <button>Subscribe to monthly newsletter</button>
+                <button type="submit" className="w-full bg-slate-800 rounded-md py-4 text-white font-medium">Subscribe to monthly newsletter</button>
 
-            <div className="pop-up" aria-hidden="true" hidden>
-                <h1>Thanks for subscribing!</h1>
+                <div className="pop-up" aria-hidden="true" hidden>
+                    <h1>Thanks for subscribing!</h1>
 
-                <p>
-                    A confirmation email has been sent to ash@loremcompany.com.
-                    Please open it and click the button inside to confirm your subscription.
-                </p>
+                    <p>
+                        A confirmation email has been sent to ash@loremcompany.com.
+                        Please open it and click the button inside to confirm your subscription.
+                    </p>
 
-                <button>Dismiss message</button>
-            </div>
+                    <button>Dismiss message</button>
+                </div>
             </main>
 
-            <footer>
+            <footer className="absolute bottom-0">
                 Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
                 Coded by <a href="#">Your Name Here</a>.
             </footer>
