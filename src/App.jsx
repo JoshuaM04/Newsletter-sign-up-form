edb4cfc294a244cc63a9dc1e9d00c468ad48d2ca
Viper <lmi25@txstate.edu>
2026-03-30 12:23:38 -0500

    Installed tailwind css, reorganized the files, and renamed the functional component that is referenced to 'Newsletter'.

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+export default function Newsletter() {
+    return (
+        <div className="bg-red-100">
+            <p>
+            Stay updated!
+
+            Join 60,000+ product managers receiving monthly updates on:
+
+            Product discovery and building what matters
+            Measuring to ensure updates are a success
+            And much more!
+
+            Email address
+            email@company.com
+
+            Subscribe to monthly newsletter
+
+            Thanks for subscribing!
+
+            A confirmation email has been sent to ash@loremcompany.com. 
+            Please open it and click the button inside to confirm your subscription.
+
+            Dismiss message
+            </p>
+            
+            <div class="attribution">
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+                Coded by <a href="#">Your Name Here</a>.
+            </div>
+        </div>
+    );
+}
