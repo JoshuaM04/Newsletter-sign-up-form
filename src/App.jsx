7cf5948421247e9307dd64da3c0fb9c98de92770
Viper <lmi25@txstate.edu>
2026-03-30 21:34:18 -0500

    Added various styling adjustments to the App.jsx file and to the root so that the content remained centered on the webpage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 export default function Newsletter() {
     return (
-        <fragment className="flex flex-col justify-center items-center min-h-screen p-5 gap-5">
-            <header className="flex flex-col">
+        <fragment className="flex flex-col justify-center items-center min-h-screen p-5 gap-5 w-xs">
+            <header className="flex flex-col w-full">
                 <div className="img-container">
                     <img src="assets/images/illustration-sign-up-mobile.svg" alt="" aria-hidden="true"/>
                 </div>
-                <h1 className="text-left">Stay updated!</h1>
+                <h1 className="text-left text-4xl font-bold">Stay updated!</h1>
             </header>
 
             <main className="flex flex-col gap-5">
@@ -18,12 +18,17 @@ export default function Newsletter() {
                 </ul>
 
                 <div>
-                    <label htmlFor="email">Email address</label>
-                    <input id="email" className="w-full border-1 rounded-md pl-5 py-4" type="text" placeholder="email@company.com" />
+                    <label htmlFor="email" className="font-medium">Email address</label>
+                    <input id="email" className="w-full border-1 rounded-md pl-5 py-4" type="text" placeholder="email@company.com" aria-required="true" />
                     <br />
                 </div>
 
-                <button type="submit" className="w-full bg-slate-800 rounded-md py-4 text-white font-medium">Subscribe to monthly newsletter</button>
+                <button type="submit" 
+                        className="w-full bg-slate-800 rounded-md py-4 text-white font-medium hover:cursor-pointer hover:bg-slate-700" 
+                        aria-haspopup="dialog"
+                >
+                    Subscribe to monthly newsletter
+                </button>
 
                 <div className="pop-up" aria-hidden="true" hidden>
                     <h1>Thanks for subscribing!</h1>
